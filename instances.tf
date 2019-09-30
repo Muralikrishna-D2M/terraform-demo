be2b33d4aaab4a1bec52d2de28e96bc6cdc7a1bc
muralikrishna Annem <MuraliKrishna.Annem@DISYS.IN>
2019-09-30 14:13:57 +0530

    cidr correction

--- instances.tf
+++ instances.tf
@@ -16,6 +16,6 @@ resource "aws_security_group_rule" "allow_all" {
   to_port         = 65535
   protocol        = "tcp"
   # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-  cidr_blocks = 0.0.0.0/0
+  cidr_blocks = [0.0.0.0/0]
   security_group_id = "sg-0798fd77acd72f318"
 }
