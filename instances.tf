9f84b693a6abe90e0299e2a751fac9c074a94d39
muralikrishna Annem <MuraliKrishna.Annem@DISYS.IN>
2019-09-30 14:05:29 +0530

    adding cidr block

--- instances.tf
+++ instances.tf
@@ -10,3 +10,12 @@ resource "aws_instance" "webserver" {
       platform = "Linux"
   }
 }
+resource "aws_security_group_rule" "allow_all" {
+  type            = "ingress"
+  from_port       = 0
+  to_port         = 65535
+  protocol        = "tcp"
+  # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+  cidr_blocks = 0.0.0.0/0
+  security_group_id = "sg-0798fd77acd72f318"
+}
